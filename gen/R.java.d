6468565863ca9946874ba1abcbafa117ede6311b
Yash Kansagara <yash.only14u@gmail.com>
2013-02-18 20:58:10 +0530

    Android app

---
+++ gen/R.java.d
@@ -0,0 +1,14 @@
+D:\Exp\Cordova\gen\com\and\yash\R.java \
+ : D:\Exp\Cordova\res\drawable\icon.png \
+D:\Exp\Cordova\res\drawable-hdpi\icon.png \
+D:\Exp\Cordova\res\drawable-hdpi\ic_launcher.png \
+D:\Exp\Cordova\res\drawable-ldpi\icon.png \
+D:\Exp\Cordova\res\drawable-ldpi\ic_launcher.png \
+D:\Exp\Cordova\res\drawable-mdpi\icon.png \
+D:\Exp\Cordova\res\drawable-mdpi\ic_launcher.png \
+D:\Exp\Cordova\res\drawable-xhdpi\icon.png \
+D:\Exp\Cordova\res\drawable-xhdpi\ic_launcher.png \
+D:\Exp\Cordova\res\layout\main.xml \
+D:\Exp\Cordova\res\values\strings.xml \
+D:\Exp\Cordova\res\xml\config.xml \
+D:\Exp\Cordova\bin\AndroidManifest.xml \
